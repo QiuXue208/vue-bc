34ba430c7b2455895724c49a732566ac534d3b96
Charles <1309571292@qq.com>
2021-06-28 23:18:04 +0800

    vue-router

---
+++ src/views/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="home">
+    此时的onpiece: {{ onepiece }} <br>
+    此时的msg: {{ msg }} <br><br>
+    <button @click="setName">设置name</button>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      name:'Kobe',
+      msg:''
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  computed: {
+    onepiece: {
+      get(){
+        return this.name +'Bryant';
+      },
+      set(newVal){
+        //当你给onepiece设置值的时候set就就会调用
+        this.msg = newVal+'is the greatest basketball player';
+      }
+    }
+  },
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+  created() {
+    console.log('created')
+  },
+  beforeMount() {
+    console.log('beforeMount')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate')
+  },
+  updated() {
+    console.log('updated')
+  },
+  destroyed() {
+    console.log('destroyed')
+  },
+  methods: {
+    setName(){
+      this.onepiece = 'JorDan'
+    }
+  },
+}
+</script>
