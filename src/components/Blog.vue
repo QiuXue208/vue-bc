a15ca04069780fe38f627ca9cb666bc88a152233
Charles <1309571292@qq.com>
2021-06-29 14:41:53 +0800

    事件触发

---
+++ src/components/Blog.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="blog-post">
+    <h3>{{ post.title }}</h3>
+    <button @click="$emit('enlarge-text', 2)">
+      Enlarge text
+    </button>
+    <div v-html="post.content"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Blog',
+    props: {
+      post: { type: Object, default: () => {}}
+    }
+  }
+</script>
---
+++ src/components/text-document.vue
@@ -0,0 +1,12 @@
+<template>
+  <button @click="$emit('update', '这是新的标题')">更改标题</button>
+</template>
+
+<script>
+  export default {
+    name: 'text-document',
+    props: {
+      title: String
+    }
+  }
+</script>
