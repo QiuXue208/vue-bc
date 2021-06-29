9fbbbf3f53c8aedb4c8054ebe6e3371275da89af
Charles <1309571292@qq.com>
2021-06-29 18:14:55 +0800

    自定义指令

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.directive('focus', {
+  inserted(el) {
+    console.log(124)
+    el.focus()
+  }
+})
+
 new Vue({
   router,
   render: h => h(App)
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h3>元素聚焦</h3>
+    <input type="text" />
+    <input type="text" v-focus />
+    <input type="text" />
   </div>
 </template>
