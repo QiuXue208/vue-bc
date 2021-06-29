dedfbbabd75fbf84c798b79e4713bc2cae667a80
Charles <1309571292@qq.com>
2021-06-29 14:41:32 +0800

    v-model

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    type="checkbox"
+    v-bind:checked="checked"
+    v-on:change="$emit('change', $event.target.checked)"
+  >
+</template>
+
+<script>
+  export default {
+    name: 'BaseCheckbox',
+    props: {
+      checked: Boolean
+    },
+    model: {
+      prop: 'checked',
+      event: 'change'
+    }
+  }
+</script>
